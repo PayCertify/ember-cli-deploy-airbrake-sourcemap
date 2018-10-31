67ce27ba285c1eaea146a1ef08d2ece5161c6417
Michael Villander <nousvillander@gmail.com>
2018-10-31 16:35:04 -0300

    Adds basic libs to create plugin

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const RSVP = require('rsvp');
+const fs = require('fs');
+const path = require('path');
+const request = require('request-promise');
+const zlib = require('zlib');
+
+const BasePlugin = require('ember-cli-deploy-plugin');
+
 module.exports = {
   name: require('./package').name
 };
